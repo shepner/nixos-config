532d85ab952a7d1c1db55263d404f6bd374f0e8c
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2024-10-30 20:06:40 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -128,4 +128,30 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  
+  fileSystems."/mnt/media" = {
+    device = "//nas.asyla.org/media";
+    fsType = "cifs";
+    options = [ "username=eddie" "password=yoursambapassword" "x-systemd.automount" "noauto" ];
+  };
+
+  # https://nixos.wiki/wiki/Samba#Samba_Client
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/media" = {
+    device = "//nas.asyla.org/media";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+  # `/etc/nixos/smb-secrets` (domain is optional)
+  # ```
+  # username=<USERNAME>
+  # domain=<DOMAIN>
+  # password=<PASSWORD>
+  # ```
+
 }
