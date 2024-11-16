ed4a7d1ac3963293f65e7c12e69583a7dcfc5efb
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2024-11-16 11:30:48 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -147,7 +147,7 @@
     options = let
       # this line prevents hanging on network split
       #automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      automount_opts = "noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
 
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
