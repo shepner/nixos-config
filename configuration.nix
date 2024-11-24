4a64181bf6d605d89f608a8a1de82fe64c08bdbf
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2024-11-24 16:32:00 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./mount.nix
     ./pkgs.nix
     ./users.nix
+    ./wm-gnome.nix
   ];
 
   # Do not change this
@@ -43,7 +44,6 @@
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -60,43 +60,6 @@
   };
 
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -110,6 +73,8 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  
+  # Enable CUPS to print documents.
+  #services.printing.enable = true;
+
   
 }
