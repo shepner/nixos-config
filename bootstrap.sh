f0bf54f4d4b25dcab46136aa8867248d660d14a2
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2024-11-24 16:31:54 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,7 @@ sudo curl -o $CONFIGDIR/configuration.nix https://raw.githubusercontent.com/shep
 sudo curl -o $CONFIGDIR/pkgs.nix https://raw.githubusercontent.com/shepner/nixos-config/refs/heads/main/pkgs.nix
 sudo curl -o $CONFIGDIR/users.nix https://raw.githubusercontent.com/shepner/nixos-config/refs/heads/main/users.nix
 sudo curl -o $CONFIGDIR/mount.nix https://raw.githubusercontent.com/shepner/nixos-config/refs/heads/main/mount.nix
+sudo curl -o $CONFIGDIR/wm-gnome.nix https://raw.githubusercontent.com/shepner/nixos-config/refs/heads/main/wm-gnome.nix
 
 # Apply new config
 sudo nixos-rebuild switch
