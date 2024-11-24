ea2e8864e00f7c258df3f8428dd4fc66419448bc
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2024-11-24 15:56:04 -0600

    Create pkgs.nix

---
+++ pkgs.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # List packages installed in system profile. To search, run `nix search wget`
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
+    docker_27
+    obsidian
+    qemu
+    google-chrome
+    firefox
+    slack
+    cifs-utils
+  ];
+}
